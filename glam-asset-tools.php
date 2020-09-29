6c45b4c9b9ebee8289255a9a53166e42eeaf0b08
andregagnon123 <contact@andregagnon.com>
2020-09-29 15:55:15 -0400

    update version

--- glam-asset-tools.php
+++ glam-asset-tools.php
@@ -16,7 +16,7 @@
  * Plugin Name:       GLAM Asset Tools
  * Plugin URI:        https://github.com/andygagnon/glam-asset-tools
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Andre Gagnon
  * Author URI:        https://andregagnon.com
  * License:           GPL-2.0+
